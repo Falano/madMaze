b76439540e381c46fe03253c5ab8e35025fcbdf0
noemie <noemie.scherer@gmail.com>
2017-09-28 11:11:44 +0200

    GUI e menus

--- madMaze/Assets/CustomAssets/Scripts/PlayerMoveOrtho.cs
+++ madMaze/Assets/CustomAssets/Scripts/PlayerMoveOrtho.cs
@@ -12,6 +12,10 @@ public class PlayerMoveOrtho : MonoBehaviour
     private Vector3[] directions = { new Vector3(0, 0, 0), new Vector3(0, 90, 0), new Vector3(0, 180, 0), new Vector3(0, 270, 0) };
     private int dir = 0;
     private bool rotating = false;
+    public Material[] mats;
+    private Color[] baseCols;
+    private Color tmpCol;
+    private float emit;
 
     void SoftRotate(int rot)
     {
@@ -19,16 +23,17 @@ public class PlayerMoveOrtho : MonoBehaviour
         dir = (dir + rot + 4) % 4;
         print("dirs number: " + dir + ", dir: " + directions[dir]);
         goalRot = Quaternion.Euler(directions[dir]);
+        baseCols = new Color[mats.Length];
+
+        for (int i = 0; i < mats.Length; i ++)
+        {
+            baseCols[i] = mats[i].color;
+        }
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            print("I'm here for you");
-            transform.rotation = goalRot;
-        }
         if (!rotating)
         {
 
@@ -52,15 +57,40 @@ public class PlayerMoveOrtho : MonoBehaviour
             }
         }
 
-        // follow-up on SoftRotate()
-        if (goalRot != transform.rotation)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRot, rotationSpeed * Time.deltaTime);
-        }
         else
         {
-            rotating = false;
-            transform.rotation = goalRot;
+            // follow-up on SoftRotate()
+            if (goalRot != transform.rotation)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRot, rotationSpeed * Time.deltaTime);
+                foreach (Material mat in mats)
+                {
+
+                    /*
+                     //change alpha (needs a standard fade shader)
+                    tmpCol = mat.color;
+                    tmpCol.a = Mathf.Abs(Mathf.Lerp(1, -1, ((transform.rotation.eulerAngles.y + 360) % 90) / 90));
+                    mat.color = tmpCol;
+                    */
+                    
+                    //change emit (doesn't work. whyyyy?)
+                    emit = 1-Mathf.Abs(Mathf.Lerp(1, -1, ((transform.rotation.eulerAngles.y + 360) % 90) / 90));
+                    tmpCol = new Color(emit, emit, emit, 1);
+                    mat.SetColor ("_EmissionColor", tmpCol);
+                }
+            }
+
+            else
+            {
+                rotating = false;
+                transform.rotation = goalRot;
+                for (int i = 0; i < mats.Length; i++)
+                {
+                    mats[i].SetColor("_EmissionColor", new Color(0,0,0,1));
+                    tmpCol = new Color(0, 0, 0, 1);
+                    //mats[i].color = baseCols[i];
+                }
+            }
         }
     }
 }
